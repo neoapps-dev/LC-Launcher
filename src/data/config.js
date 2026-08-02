79311450c3f3acd9e63732abdecb81f2c3a58d8c
TheHuckle <crazyh.dev@gmail.com>
2026-08-03 00:36:05 +0100

    add temporary update solution

--- src/data/config.js
+++ src/data/config.js
@@ -1,9 +1,10 @@
 export default {
+    projectGithubUser: "thehuckledev",
+    projectGithubRepo: "lc-launcher",
     /*apiDomain: "https://api.lcelauncher.com",*/
     apiDomain: "htts://localhost:8080",
     discordInvite: ["discord://-/invite/DBw6V5v8C9", "https://discord.gg/DBw6V5v8C9"],
     defaultInstance: "neo-legacy",
-    updateURL: "https://update.lclauncher.huckle.dev/req/v1/?m=true",
 
     rpcClientID: "1493892302312050789",
     rpcIcon: "icon",
--- src/utils/updater.js
+++ src/utils/updater.js
@@ -2,9 +2,50 @@ import Neutralino from "@neutralinojs/lib";
 import config from "../data/config.js";
 
 import { showAlert } from "../components/Alert.jsx";
+import Net from "../lib/net.js";
+
+const isVersionGreater = (latest, current) => {
+    const clean = (v) => v.split("-")[0].replace("v", "").split(".").map(Number);
+    const [l1, l2, l3] = clean(latest);
+    const [c1, c2, c3] = clean(current);
+
+    if (l1 !== c1) return l1 > c1;
+    if (l2 !== c2) return l2 > c2;
+    return l3 > c3;
+};
 
 export const checkForUpdates = async() => {
-    return; // No auto update for now, needs a whole rework and currently people are being blocked leading to frozen setup screen
+    const latestRelease = await Net.get(`https://api.github.com/repos/${config.projectGithubUser}/${config.projectGithubRepo}/releases/latest`, {
+        headers: {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2026-03-10'
+        }
+    });
+
+    if (latestRelease?.ok !== true) {
+        console.error("Failed to check for updates:", data);
+        return showToast("Error: Failed to check for updates");
+    };
+
+    if (!isVersionGreater(latestRelease?.data?.tag_name, NL_APPVERSION))
+        return console.log("LC Launcher is up to date!");
+
+    let openPage = await showAlert(
+        'LC Launcher Update',
+        `Would you like to update LC Launcher?
+
+From v${NL_APPVERSION} to ${latestRelease?.data?.tag_name}${latestRelease?.data?.body ? `\n\nRelease Notes:\n${latestRelease?.data?.body}` : ""}`,
+        'YES_NO',
+        "LEFT"
+    );
+    if (openPage === 'YES') {
+        await Neutralino.os.open(latestRelease?.data?.html_url);
+
+        if (window.whenQuitting) await window.whenQuitting();
+        await Neutralino.app.exit();
+    };
+
+    // No auto update for now, needs a whole rework and currently people are being blocked leading to frozen setup screen
     /*if (NL_ARGS.includes("--neu-dev-extension")) return console.log("Update check bypassed, dev mode is on!");
     if (navigator.onLine === false) return;
 
